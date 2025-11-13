ee613e6c104d6b2a6b9d985509a070ef21c19316
faruk-95 <farukchesap1@gmail.com>
2025-11-13 09:36:44 +0100

    Update include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Navbar'ı yükle
+  // Navbar load
   fetch("components/navbar.html")
     .then(response => response.text())
     .then(data => {
       document.body.insertAdjacentHTML("afterbegin", data);
     });
 
-  // Footer'ı yükle
+  // Footer load
   fetch("components/footer.html")
     .then(response => response.text())
     .then(data => {
